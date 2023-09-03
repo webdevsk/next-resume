d552f1852d89917a32dd987c6c2dd4dcccd13fc5
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-09-03 14:10:36 +0600

    Added Resume data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import resume from "./resume"
+const { data } = resume
+
 function App() {
   const url = new URL(window.location.href)
   const params = new URLSearchParams(url.search)
@@ -7,14 +10,8 @@ function App() {
     <div className={!resumeOnlyMode ? "flex justify-center py-16" : ""}>
       {/* {!resumeOnlyMode && <div id="other-body-stuff">Hi stuff goes here</div>} */}
       <div id="pdf">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <h4>Hello World</h4>
-        <h5>Hello World</h5>
-        <h6>Hello World</h6>
-        <p>Hello World</p>
-        <small>Hello World</small>
+        <h1>{data?.firstName}</h1>
+        <h1>{data?.lastName}</h1>
       </div>
     </div>
   )
---
+++ src/resume.js
@@ -0,0 +1,74 @@
+const resume = {
+  data: {
+    firstName: "Mohammed Salman",
+    lastName: "Khan",
+    photo: "",
+    title: "Junior Frontend Web Developer",
+    location: "Dhaka, Bangladesh",
+    phone: "+880 1305 335 088",
+    email: "webdevz.sk@gmail.com",
+    telephone: null,
+    github: "webdevzsk",
+    linkedin: "webdevzsk",
+    twitter: null,
+    facebook: null,
+    about:
+      "I am a Frontend Web Developer creating interactable solutions and solving both technical and design challenges using Vanilla JavaScript and React JS. I have more than 2 years of experience in creating pixel-perfect and mobile responsive Websites that met client requirements. I debug design and functional issues with my deep knowledge on Dev Tools and Search Engines.",
+    skills: [
+      {
+        heading: "Frontend",
+        data: [
+          "React",
+          "Vanilla JavaScript",
+          "Tailwind",
+          "Sass",
+          "CSS",
+          "HTML",
+        ],
+      },
+      {
+        heading: "Backend",
+        data: [
+          "Express JS",
+          "Node JS",
+          "MongoDB",
+          "MySQL",
+          "Serverless Functions",
+          "Restful API",
+        ],
+      },
+      {
+        heading: "Technologies",
+        data: ["Git", "GitHub", "WordPress", "Adobe Xd", "Figma"],
+      },
+    ],
+    projects: [
+      //   {
+      //     title: null,
+      //     link: null,
+      //     skills: [],
+      //     responsibilities: [],
+      //   },
+    ],
+    experiences: [
+      //   {
+      //     position: null,
+      //     company: null,
+      //     location: null,
+      //     dateFrom: null,
+      //     dateTo: null,
+      //     responsibilities: [],
+      //   },
+    ],
+    educations: [
+      //   {
+      //     title: null,
+      //     institute: null,
+      //     dateFrom: null,
+      //     dateTo: null,
+      //   },
+    ],
+  },
+}
+
+export default resume
